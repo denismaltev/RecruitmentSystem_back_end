7929ebc70df6024de9aaaf1f3edcd701c5bd3a77
AnnaBerman <aberman2@my.bcit.ca>
2020-04-16 22:09:22 -0700

    #16 job controller - only for company role GET - get list of jobs of logined user's company GET by job Id POST PUT

---
+++ RecruitmentSystemAPI/Controllers/JobsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecruitmentSystemAPI.Models;
+using RecruitmentSystemAPI.Repositories;
+using RecruitmentSystemAPI.ViewModels;
+
+namespace RecruitmentSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class JobsController : ControllerBase
+    {
+        private readonly RecruitmentSystemContext _context;
+        private readonly UserManager<SystemUser> _userManager;
+
+        public JobsController(RecruitmentSystemContext context, UserManager<SystemUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Jobs
+        [HttpGet]
+        [Authorize(Roles = "Company")]
+        public ActionResult<IEnumerable<JobVM>> GetJobs()
+        {
+            var jobRepo = new JobRepo(_context);
+            var userId = _userManager.GetUserId(User);
+            var result = jobRepo.GetCompanyJobsByUserId(userId);
+            return Ok(result);
+        }
+
+        // GET: api/Jobs/5
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Company")]
+        public ActionResult<Job> GetJob(int id)
+        {
+            var jobRepo = new JobRepo(_context);
+            var result = jobRepo.GetJobById(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        // PUT: api/Jobs/5
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Company")]
+        public IActionResult PutJob(int id, JobVM jobVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != jobVM.Id)
+                {
+                    return BadRequest();
+                }
+
+                var jobRepo = new JobRepo(_context);
+                try
+                {
+                    jobRepo.UpdateJob(jobVM);
+                    return Ok();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!jobRepo.JobExists(id))
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                    return NotFound();
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(500, new { status = 500, message = e.Message });
+                }
+
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        // POST: api/Jobs
+        [HttpPost]
+        [Authorize(Roles = "Company")]
+        public ActionResult<Job> PostJob([FromBody]JobVM jobVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var jobRepo = new JobRepo(_context);
+                    var userId = _userManager.GetUserId(User);
+                    var result = jobRepo.AddJob(jobVM, userId);
+                    return Ok(result);
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(500, new { message = e.Message });
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ RecruitmentSystemAPI/Models/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystemAPI.Models
+{
+    [Flags]
+    public enum Weekdays 
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 8,
+        Thursday = 16,
+        Friday = 32,
+        Saturday = 64,
+    }
+}
