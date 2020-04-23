9a4f2d09a2110a8a2c3be163e7f4787bde0d41b2
thaqueubc <reya.ruet@gmail.com>
2020-04-22 22:39:40 -0700

    added jobrating in Job table

---
+++ RecruitmentSystemAPI/Migrations/20200423052145_addJobRating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecruitmentSystemAPI.Migrations
+{
+    public partial class addJobRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JobRating",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobRating",
+                table: "Jobs");
+        }
+    }
+}
