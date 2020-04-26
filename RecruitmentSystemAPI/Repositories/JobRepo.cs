c65537b3ede98d6c807d113db884b8a6bd91129a
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-26 00:50:34 -0700

    Added missing rating field to method in repo issue #73

--- RecruitmentSystemAPI/Repositories/JobRepo.cs
+++ RecruitmentSystemAPI/Repositories/JobRepo.cs
@@ -25,7 +25,8 @@ namespace RecruitmentSystemAPI.Repositories
             {
                 CompanyId   = j.CompanyId,
                 CompanyName = j.Company.Name,
-                Title       = j.Title
+                Title       = j.Title,
+                Rating      = j.Rating
             });
         }
 
