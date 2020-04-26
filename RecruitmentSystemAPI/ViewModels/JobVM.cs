5b5190319dc542241dbb9d39e50a7e0c47c13d99
Sara <sbanaeirad@my.bcit.ca>
2020-04-26 13:27:32 -0700

    #47 revert change to jobvm

--- RecruitmentSystemAPI/ViewModels/JobVM.cs
+++ RecruitmentSystemAPI/ViewModels/JobVM.cs
@@ -12,8 +12,6 @@ namespace RecruitmentSystemAPI.ViewModels
         [Required]
         public int CompanyId { get; set; }
 
-        public string CompanyName { get; set; }
-
         public string Title { get; set; }
         public string Description { get; set; }
         [Required]
