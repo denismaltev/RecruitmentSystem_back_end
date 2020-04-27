f3807e888296ff51540d746c0010cfffb2870f4e
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-26 23:43:08 -0700

    Added job labourer view model for the list needed of job labourers. Issue #51

---
+++ RecruitmentSystemAPI/ViewModels/JobLabourerVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystemAPI.ViewModels
+{
+    public class JobLabourerVM
+    {
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SkillName { get; set; }
+    }
+}
