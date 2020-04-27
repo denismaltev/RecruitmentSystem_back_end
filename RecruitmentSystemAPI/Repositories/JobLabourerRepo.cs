fc1d7451e370fa330b1d81617228fc5f99bbad35
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-27 13:10:55 -0700

    Fixed format of queries based on feedback

--- RecruitmentSystemAPI/Repositories/JobLabourerRepo.cs
+++ RecruitmentSystemAPI/Repositories/JobLabourerRepo.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RecruitmentSystemAPI.Models;
 using RecruitmentSystemAPI.ViewModels;
 using System;
@@ -19,34 +20,12 @@ namespace RecruitmentSystemAPI.Repositories
 
         public IQueryable<JobLabourerVM> GetLabourersList(int jobId)
         {
-            // first name + last name + phone on Labourer table where labourer.id == labourerJobs.labourerId and labourerJobs.JobId == Jobs.Id 
-            //and
-            //select skills.Name where skills.id == labourerjobs.skillId and jobId == jobs.id 
-            var fullName = _context.LabourerJobs
-                .Where(l => l.JobId == jobId)
-                .Where(l => l.LabourerId == l.Labourer.Id)
-                .Select(l => new { l.Labourer.FirstName, l.Labourer.LastName }).FirstOrDefault().ToString();
-
-            var phoneNumber = _context.LabourerJobs
-                .Where(l => l.JobId == jobId)
-                .Where(l => l.LabourerId == l.Labourer.Id)
-                .Select(l => l.Labourer.Phone).FirstOrDefault();
-
-            var skillName = _context.LabourerJobs
-                .Where(l => l.JobId == jobId)
-                .Where(l => l.LabourerId == l.Labourer.Id)
-                .Where(l => l.SkillId == l.Skill.Id)
-                .Select(s => s.Skill.Name).FirstOrDefault();
-
-            //.Where(j => j.Id == _context.LabourerJobs.)
-
-            return _context.Jobs.Select(j => new JobLabourerVM
-            {
-                JobId = j.Id,
-                FullName = fullName,
-                PhoneNumber = phoneNumber,
-                SkillName = skillName
-            }); 
+            return _context.LabourerJobs
+               .Where(l => l.JobId == jobId)
+               .Include(l => l.Labourer)
+               .Include(l => l.Skill)
+               .Where(l => l.LabourerId == l.Labourer.Id)
+               .Select(l => new JobLabourerVM { JobId = l.JobId, FullName = l.Labourer.FirstName + " " + l.Labourer.LastName, PhoneNumber = l.Labourer.Phone, SkillName = l.Skill.Name });
         }
     }
 }
