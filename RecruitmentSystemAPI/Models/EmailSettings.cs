c52ab685e116f019c17ba1538b4ff146b09b7b40
Den79 <test08081979@gmail.com>
2020-04-27 19:32:50 -0700

    #71 EmailSettings and appsettings

---
+++ RecruitmentSystemAPI/Models/EmailSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystemAPI.Models
+{
+    public class EmailSettings
+
+    {
+        public string Domain { get; set; }
+        public int Port { get; set; }
+        public string UsernameLogin { get; set; }
+        public string UsernamePassword { get; set; }
+        public string FromEmail { get; set; }
+        public string DisplayName { get; set; }
+        public string ToEmail { get; set; }
+        public string[] BccEmails { get; set; }
+    }
+}
