8ce12ca40c68aecf09cc9a2d7e557ac84eeab0d4
AnnaBerman <aberman2@my.bcit.ca>
2020-04-29 21:46:51 -0700

    #85 added optional companyId to GET labourer jobs

--- RecruitmentSystemAPI/Repositories/LabourerJobsRepo.cs
+++ RecruitmentSystemAPI/Repositories/LabourerJobsRepo.cs
@@ -31,7 +31,7 @@ namespace RecruitmentSystemAPI.Repositories
                    PhoneNumber  = l.Labourer.Phone, SkillName = l.Skill.Name });
         }
 
-        public IQueryable<LabourerJobVM> GetLabourerJobsByUserRole(ClaimsPrincipal user, int count, int page, int? jobId, int? labourerId, out int totalRows, DateTime? fromDate = null, DateTime? toDate = null)
+        public IQueryable<LabourerJobVM> GetLabourerJobsByUserRole(ClaimsPrincipal user, int count, int page, int? jobId, int? labourerId, int? companyId, out int totalRows, DateTime? fromDate = null, DateTime? toDate = null)
         {
             var userId = _userManager.GetUserId(user);
             var query = _context.LabourerJobs
@@ -52,10 +52,13 @@ namespace RecruitmentSystemAPI.Repositories
             {
                 query = query.Where(l => l.JobId == jobId.Value);
             }
-
-            else if (labourerId.HasValue)
+            if (labourerId.HasValue)
             {
                 query = query.Where(l => l.LabourerId == labourerId);
+            }
+            if (companyId.HasValue)
+            {
+                query = query.Where(l => l.Job.CompanyId == companyId);
             }
 
 
